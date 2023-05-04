be64ed242ede794da751e514d58658cac56816bc
Jusa <105008598+Jose010101@users.noreply.github.com>
2023-05-04 16:39:10 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,13 +1,16 @@
 <?php
-$server = "servidortcc0405.database.windows.net";
-$user = "adm";
-$password = "Senai260";
-$bd = "fluxocaixa";
-
+// PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("mysql:host=$server;dbname=$bd", $user, $password);
+    $conn = new PDO("sqlsrv:server = tcp:servidortcc0405.database.windows.net,1433; Database = fluxocaixa", "adm", "{your_password_here}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "adm", "pwd" => "{your_password_here}", "Database" => "fluxocaixa", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:servidortcc0405.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
