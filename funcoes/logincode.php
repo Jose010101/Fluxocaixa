255043a9931fd92d8c08f094cb2355493a67888f
Jusa <105008598+Jose010101@users.noreply.github.com>
2023-05-04 17:15:49 -0300

    Update logincode.php

--- funcoes/logincode.php
+++ funcoes/logincode.php
@@ -1,8 +1,8 @@
 <?php
-include_once("../conexao.php");
+//include_once("../conexao.php");
 
-$codigo = $_POST['codigo_user'];
-$senha = $_POST['senha_user'];
+//$codigo = $_POST['codigo_user'];
+//$senha = $_POST['senha_user'];
 
 // Verifica se todos os campos foram preenchidos
 if(isset($_POST['codigo_user']) && isset($_POST['senha_user'])){
